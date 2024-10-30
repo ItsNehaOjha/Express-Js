3f5b3577a414737041d12120abeadde7dc3eb89d
Neha <ojhaneha0028@gmail.com>
2024-10-30 20:06:14 +0530

    cookies and session and about CRUD me se C aur R ke bare me bas

--- 03_EndGameOneExpGENERTOR/routes/index.js
+++ 03_EndGameOneExpGENERTOR/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 const userModel = require("./users")
 
 router.get('/', function(req, res, next) {
+  req.session.koibhinaam ="hello";
   res.render('index');
 });
+//can check the session in any other route: 
+router.get('/checkSession', function(req,res){
+  console.log(req.session);
+  res.send("Check kiya h console dekho");
+})
 
-//create update delete read(CRUD)
+//create update delete read(CRUD) : 
+//create :
 
+//NOTE: agar ye function likha rha to jitni bar me server bnaungi nya same user bnta jayega 
 router.get("/create", async function(req,res){
   const createdUser = await userModel.create({
     username: "Neha",
@@ -17,4 +25,28 @@ router.get("/create", async function(req,res){
   res.send(createdUser);
 });
 
+
+// read user
+
+router.get("/allusers",async function(req,res){
+  let allUsers= await userModel.find();  //.find = sare bande nikal k bhejta h
+  res.send(allUsers);   //these data is in a array
+})
+
+//.findOne method to read one banda
+
+// router.get("/allusers",async function(req,res){
+//   let allUsers= await userModel.findOne({username:"Neha"});  
+//   res.send(allUsers);   
+// })
+
+
+//delete user
+
+router.get("/delete",async function(req,res){
+    let deletedUser = await userModel.findOneAndDelete({
+      username:"Neha"
+    })
+    res.send(deletedUser);
+})
 module.exports = router;
